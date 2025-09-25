0bafbf2348194ef7bb7a452876812331c72b7ea2
SannyBhar <sankeerthbharadwaj@gmail.com>
2025-09-25 12:30:15 -0400

    Initial Commit

---
+++ HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Sankeerth Bharadwaj on 9/25/25.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
